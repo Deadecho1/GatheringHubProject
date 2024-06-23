bc2f5ffb276e0a2e909dc6e2d4b0279012ac4443
Deadecho1 <167852254+Deadecho1@users.noreply.github.com>
2024-06-23 22:03:19 +0300

    chat features
    working on chat features

--- js/map.js
+++ js/map.js
@@ -13,16 +13,17 @@ document.addEventListener('userDataReady', async function () {
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("chat-toggle-button").addEventListener("click", () => {
         const chatContainer = document.getElementById("chat-box");
-        const rotateButton = document.getElementById("chat-toggle-button");
+        const toggleButton = document.getElementById("chat-toggle-button");
+        const inputForm = document.getElementById("chat-send");
         chatContainer.classList.toggle("expanded");
-        rotateButton.classList.toggle("rotated");
+        toggleButton.classList.toggle("rotated");
+        toggleButton.classList.toggle("expanded");
+        inputForm.classList.toggle("visible");
         if (chatContainer.classList.contains("expanded")) {
         } else {
         }
     });
 });
-
-
 
 function loadAvatar(playerName, avatarName, nameColor, screenLeftPercent, screenTopPercent){
     try {
