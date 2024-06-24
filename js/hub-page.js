5d05a70d033a385c09c5ffd91f6cdfe996af50cd
yaelrosenblat <165557865+yaelrosenblat@users.noreply.github.com>
2024-06-25 01:14:27 +0300

    json
    הכנסת data מהjson

--- js/hub-page.js
+++ js/hub-page.js
@@ -1,5 +1,24 @@
-let temporaryQueryStringHub;
+document.addEventListener('DOMContentLoaded', () => {
+    const backButton = document.getElementById('x-icon');
 
+    backButton.addEventListener('click', () => {
+        history.back();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data/hubs.json')
+        .then(response => response.json()) 
+        .then(data => {
+            const name = data[4].name;
+            const address = data[4].location;
+            const heading = document.querySelector('h1');
+            const hubPageAddress = document.getElementById('address');
+            heading.textContent = name;
+            hubPageAddress.textContent = address;
+        })
+    .catch(error => console.error('Error fetching JSON data:', error));
+});
 
 
 
