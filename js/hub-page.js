e968343ab5db72b8484e51dd62575a98d7e1444d
yaelrosenblat <165557865+yaelrosenblat@users.noreply.github.com>
2024-06-26 22:36:23 +0300

    javascript: open, name, phone, address.
    רק לlevel up

--- js/hub-page.js
+++ js/hub-page.js
@@ -1,3 +1,50 @@
+document.addEventListener('userDataReady', () => {
+    try {
+        // Now that userData and hubsData are ready, you can use them here
+        console.log('userData:', userData);
+        console.log('hubsData:', hubsData);
+        // window.location.reload();
+        setSimpleSetUp();
+        
+    } catch (error) {
+        handleError(error, "Failed to initialize list");
+    }
+});
+
+function setSimpleSetUp(){
+    document.querySelector('h1').textContent = hubsData[4].name;
+    document.querySelector('h2#address').textContent = hubsData[4].location;
+    document.querySelector('h2#phone-number').textContent = hubsData[4].phone;
+    setupHubStatus(hubsData[4]);
+}
+
+
+function setupHubStatus(hub) {
+    let statusH2 = document.getElementById('text-open-close');
+    let isOpen = checkOpenStatus(hub.openingHour, hub.closingHour);
+    statusH2.textContent = isOpen ? "Open" : "Closed";
+    statusH2.classList.add(isOpen ? "text-open" : "text-close");
+}
+
+
+
+function checkOpenStatus(openingHour, closingHour) {
+    const now = new Date();
+    const currentTime = addLeadingZero(now.getHours()) + addLeadingZero(now.getMinutes());
+
+    if (closingHour < openingHour) {
+        closingHour = String(Number(closingHour) + 2400);
+    }
+
+    return currentTime < closingHour && currentTime > openingHour;
+}
+
+function addLeadingZero(num) {
+    return num < 10 ? `0${num}` : String(num);
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const backButton = document.getElementById('x-icon');
 
@@ -5,20 +52,5 @@ document.addEventListener('DOMContentLoaded', () => {
         history.back();
     });
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('data/hubs.json')
-        .then(response => response.json()) 
-        .then(data => {
-            const name = data[4].name;
-            const address = data[4].location;
-            const heading = document.querySelector('h1');
-            const hubPageAddress = document.getElementById('address');
-            heading.textContent = name;
-            hubPageAddress.textContent = address;
-        })
-    .catch(error => console.error('Error fetching JSON data:', error));
-});
-
 
 
