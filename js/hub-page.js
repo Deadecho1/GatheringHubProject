14665c8f401d01a7538198c9baf51f5f4a25e869
yaelrosenblat <165557865+yaelrosenblat@users.noreply.github.com>
2024-06-27 12:05:14 +0300

    location work
    המיקום מביא לגוגל מפות

--- js/hub-page.js
+++ js/hub-page.js
@@ -1,21 +1,22 @@
+
 document.addEventListener('userDataReady', () => {
     try {
-        // Now that userData and hubsData are ready, you can use them here
-        console.log('userData:', userData);
         console.log('hubsData:', hubsData);
         // window.location.reload();
-        setSimpleSetUp();
+        SetUpPage();
+        
         
     } catch (error) {
         handleError(error, "Failed to initialize list");
     }
 });
 
-function setSimpleSetUp(){
+function SetUpPage() {
     document.querySelector('h1').textContent = hubsData[4].name;
     document.querySelector('h2#address').textContent = hubsData[4].location;
     document.querySelector('h2#phone-number').textContent = hubsData[4].phone;
     setupHubStatus(hubsData[4]);
+    
 }
 
 
@@ -25,8 +26,6 @@ function setupHubStatus(hub) {
     statusH2.textContent = isOpen ? "Open" : "Closed";
     statusH2.classList.add(isOpen ? "text-open" : "text-close");
 }
-
-
 
 function checkOpenStatus(openingHour, closingHour) {
     const now = new Date();
@@ -43,7 +42,13 @@ function addLeadingZero(num) {
     return num < 10 ? `0${num}` : String(num);
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const backButton = document.getElementById('x-icon');
 
+    backButton.addEventListener('click', () => {
+        history.back();
+    });
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     const backButton = document.getElementById('x-icon');
@@ -53,4 +58,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const navButtons = document.getElementsByClassName('location-icon');
+
+    Array.from(navButtons).forEach(button => {
+        button.addEventListener('click', () => {
+            const latitude = hubsData[4].mapCoordinates[0]; // Replace with the actual latitude
+            const longitude = hubsData[4].mapCoordinates[1]; // Replace with the actual longitude
+            const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
+            window.open(url, '_blank');
+        });
+    });
+});
 
